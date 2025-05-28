73e3e22a96a57a9e4de87002d5869008948debfe
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-05-28 14:45:42 +0000

    01-12

--- js/gameScene.js
+++ js/gameScene.js
@@ -11,7 +11,7 @@ class GameScene extends Phaser.Scene {
         const alienXLocation = Math.floor(Math.random() * 1920) + 1 // # between 1 and 1920
         let alienXVelocity = Math.floor(Math.random() * 50) + 1 // # between 1 and 50
         alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
-        const anAlien = this.physics.add.sprite(100, 100, 'alien')
+        const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
         anAlien.body.velocity.y = 200
         anAlien.body.velocity.x = alienXVelocity
         this.alienGroup.add(anAlien)
