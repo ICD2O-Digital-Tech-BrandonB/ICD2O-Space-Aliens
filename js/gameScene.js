b1792ec78be0ec6acfc99f9be92e5927a81e4af8
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-05-30 14:58:18 +0000

    01-16

--- js/gameScene.js
+++ js/gameScene.js
@@ -26,7 +26,10 @@ class GameScene extends Phaser.Scene {
         this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
         
         this.gameOverText = null
-        this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center'}
+        this.gameOverTextStyle = { font: '65px Arial', fill: '#ff0000', align: 'center' }
+        
+        this.isGameOver = false
+        this.isGameStart = false
 
     }
   
@@ -77,7 +80,7 @@ class GameScene extends Phaser.Scene {
             this.physics.pause()
             alienCollide.destroy()
             shipCollide.destroy()
-            this.fireMissile = true
+            this.isGameOver = true
             this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over! Click to play again.', this.gameOverTextStyle).setOrigin(0.5)
             this.gameOverText.setInteractive({ useHandCursor: true })
             this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
@@ -128,6 +131,12 @@ class GameScene extends Phaser.Scene {
         if (keySpaceObj.isDown === true) {
             if (this.fireMissile === false) {
                 this.fireMissile = true
+                if (this.fireMissile === false && !this.isGameOver) {
+                    this.fireMissile = false
+                }
+                if (this.fireMissile === false && !this.isGameStart) {
+                    this.fireMissile = true
+                }
                 const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
                 this.missileGroup.add(aNewMissile)
                 this.sound.play('laser')
