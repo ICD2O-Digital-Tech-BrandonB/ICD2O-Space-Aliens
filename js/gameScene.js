e9eec47a5ee6979c2665151feb2fad6f7feb9db7
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-05-30 14:44:56 +0000

    01-16

--- js/gameScene.js
+++ js/gameScene.js
@@ -77,7 +77,7 @@ class GameScene extends Phaser.Scene {
             this.physics.pause()
             alienCollide.destroy()
             shipCollide.destroy()
-            this.fireMissile = false
+            this.fireMissile = true
             this.gameOverText = this.add.text(1920 / 2, 1080 / 2, 'Game Over! Click to play again.', this.gameOverTextStyle).setOrigin(0.5)
             this.gameOverText.setInteractive({ useHandCursor: true })
             this.gameOverText.on('pointerdown', () => this.scene.start('gameScene'))
