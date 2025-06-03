2dfd1bc57ac5cd1e9f13a49249178e645566cbc5
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-06-03 14:45:49 +0000

    01-16 Exc

--- js/gameScene.js
+++ js/gameScene.js
@@ -161,10 +161,9 @@ class GameScene extends Phaser.Scene {
                 }
             })
             this.powerText = this.add.text(1920 / 2, 1080 / 2, 'Power UP!\nYou now shoot 50% faster!', this.powerTextStyle).setOrigin(0.5)
-            setTimeout(() => {
-                this.powerText.style.display = 'none';
-            }, 3000)
-
+            if (time > 3000){
+                this.powerText.display = 'none';
+            }
         }
     }
 }
